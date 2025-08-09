3295d5da1c41716c4bc5bf72b882e9f1e70b0b90
Sami Shuraim <89112708+SamiShuraim@users.noreply.github.com>
2025-08-09 15:07:10 +0300

    Add migrations

---
+++ Migrations/20250805191859_Node Geometry instead of Location.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ai_indoor_nav_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class NodeGeometryinsteadofLocation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Location",
+                table: "route_nodes",
+                newName: "Geometry");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_route_nodes_Location",
+                table: "route_nodes",
+                newName: "IX_route_nodes_Geometry");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Geometry",
+                table: "route_nodes",
+                newName: "Location");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_route_nodes_Geometry",
+                table: "route_nodes",
+                newName: "IX_route_nodes_Location");
+        }
+    }
+}
